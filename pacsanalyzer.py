a09743b1b73521d2d8f8d6117664496c8d5eada8
Bernd Brägelmann <bernd@braegelmann.net>
2022-12-27 22:22:43 +0100

    erste version geht

--- pacsanalyzer.py
+++ pacsanalyzer.py
@@ -2,6 +2,15 @@ import argparse
 from pydicom.dataset import Dataset
 from pynetdicom import AE, debug_logger
 from pynetdicom.sop_class import PatientRootQueryRetrieveInformationModelFind
+import pandas as pd
+from tabulate import tabulate
+import re
+import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+
+ds = Dataset()
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("host", help="Hostaddress of the pacs server",
@@ -11,7 +20,7 @@ parser.add_argument("port", help="Port of the pacs server",
 # parser.add_argument("aet", help="AET of the pacs server",
 #                     type=str)
 args = parser.parse_args()
-#print("Querying :",args.host)
+print("Querying :",args.host)
 
 #debug_logger()
 
@@ -19,13 +28,18 @@ ae = AE()
 ae.add_requested_context(PatientRootQueryRetrieveInformationModelFind)
 
 # Create our Identifier (query) dataset
-ds = Dataset()
 ds.QueryRetrieveLevel = 'SERIES'
-ds.StudyDate = '20220819'
-ds.StudyDescription = '*'
+#ds.StudyDate = '202211*'
+ds.StudyDate = '2021*'
+ds.StudyDescription = ''
 ds.StudyTime = ''
 #ds.PatientName = '*'
-ds.InstitutionName = '*'
+ds.InstitutionName = ''
+
+
+#print(tabulate(df))
+
+#quit();
 
 queryresult=[]
 # Associate with the peer AE at IP 127.0.0.1 and port 11112
@@ -45,5 +59,36 @@ if assoc.is_established:
 else:
     print('Association rejected, aborted or never connected')
 
-for(ds) in queryresult:
-    print(ds.StudyDescription)
+#print("Total count of Series on PACS: ",len(queryresult))
+
+
+timestamps = []
+workingdays = []
+for result in queryresult:
+    datestring=result[0x00080020].value + result[0x00080030].value
+    datestring=re.sub(r'\..*' ,"",datestring)
+#    print(datestring)
+    timestamp=datetime.datetime.strptime(datestring, "%Y%m%d%H%M%S")
+    timestamps.append(timestamp)
+
+    if int(timestamp.strftime('%w'))>4:
+        workingdays.append(False)
+    else:
+        workingdays.append(True)
+    
+df = pd.DataFrame(data={"Timestamp": timestamps, "Workingday":workingdays})
+
+workingdays=df[df['Workingday']==True]
+nonworkingdays=df[df['Workingday']==False]
+
+#df["Timestamp"]=df["Timestamp"].astype("datetime64")
+#df.groupby(df["Timestamp"].dt.hour).count().plot(kind="bar")
+
+
+workingdays["Timestamp"]=df["Timestamp"].astype("datetime64")
+workingdays.groupby(workingdays["Timestamp"].dt.hour).count().plot(title="Working Days",kind="bar")
+
+nonworkingdays["Timestamp"]=df["Timestamp"].astype("datetime64")
+nonworkingdays.groupby(nonworkingdays["Timestamp"].dt.hour).count().plot(title="Non Working Days",kind="bar")
+
+plt.show()
